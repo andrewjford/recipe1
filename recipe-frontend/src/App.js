48888615d2cfae0a38d82d215c2bd467beeac4ea
AJ <ajoeford@gmail.com>
2017-08-07 13:38:21 -0700

    front end linked with just react

--- recipe-frontend/src/App.js
+++ recipe-frontend/src/App.js
@@ -4,15 +4,36 @@ import Recipes from './containers/Recipes'
 import { connect } from 'react-redux';
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      recipes: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/recipes')
+    .then(response => response.json())
+    .then(array => this.setState({...this.state, recipes: array}))
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React</h2>
         </div>
-        <Recipes />
+        <Recipes recipes={this.state.recipes}/>
       </div>
     );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    recipes: state.recipes
   }
 }
 
--- recipe-frontend/src/containers/Recipes.js
+++ recipe-frontend/src/containers/Recipes.js
@@ -6,13 +6,13 @@ class Recipes extends React.Component {
 
   render() {
 
-    // const recipes = this.state.recipes.map((recipe, index) => {
-    //   return <Recipe key={index} recipe={recipe}/>
-    // })
+    const recipes = this.props.recipes.map((recipe, index) => {
+      return <Recipe key={index} recipe={recipe}/>
+    })
 
     return <div>
       <h1>Recipes container</h1>
-      {/* {recipes} */}
+      {recipes}
     </div>
   }
 }

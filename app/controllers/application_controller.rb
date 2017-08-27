706eff78e8bfca98f124e25720c5058adac67774
AJ <ajoeford@gmail.com>
2017-08-27 15:07:52 -0700

    middle of adding auth tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -13,5 +16,16 @@ class ApplicationController < ActionController::Base
     @ingredients = Ingredient.all
     @recipes = Recipe.all
     render 'home/index'
+  end
+
+  def token
+    authenticate_with_http_basic do |email, password|
+      user = User.find_by(email: email)
+      if user && user.authenticate(password)
+        render json: {token: user.auth_token}
+      else
+        render json: {error: 'Incorrect credentials'}, status: 401
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  get '/token' => 'application#token'
 end
